6bf9630a13dcec3e7b4c1a1c19f0a81111d8e17c
Piotr <piotr.wolski@telecom-paristech.fr>
2020-11-22 10:29:20 -0500

    Add datadog profiler

--- main.go
+++ main.go
@@ -9,6 +9,7 @@ import (
 
 	"github.com/piochelepiotr/minecraftGo/game"
 	"github.com/piochelepiotr/minecraftGo/game_engine/render"
+	"gopkg.in/DataDog/dd-trace-go.v1/profiler"
 )
 
 const windowWidth = 800
@@ -18,6 +19,15 @@ func main() {
 	go func() {
 		log.Println(http.ListenAndServe("localhost:6060", nil))
 	}()
+	err := profiler.Start(
+		profiler.WithService("minecraft-go"),
+		profiler.WithEnv("dev"),
+		profiler.WithVersion("v0.5"),
+	)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer profiler.Stop()
 	rand.Seed(time.Now().UnixNano())
 	d := render.NewDisplay(windowWidth, windowHeight)
 	defer d.CloseDisplay()
